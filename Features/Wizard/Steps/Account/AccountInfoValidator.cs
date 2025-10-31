c21651cf44cd728862623223665acd6d68d28a92
Koczo <koczus@gmail.com>
2025-11-01 00:27:40 +0100

    Enhance validation messages for account and personal information in setup wizard

--- Features/Wizard/Steps/Account/AccountInfoValidator.cs
+++ Features/Wizard/Steps/Account/AccountInfoValidator.cs
@@ -6,8 +6,16 @@ public class AccountInfoValidator : AbstractValidator<AccountInfo>
 {
     public AccountInfoValidator()
     {
-        RuleFor(x => x.Username).NotEmpty().MinimumLength(3);
-        RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
-        RuleFor(x => x.ConfirmPassword).NotEmpty().Equal(x => x.Password);
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Username is required")
+            .MinimumLength(3).WithMessage("Username must be at least 3 characters");
+        
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+        
+        RuleFor(x => x.ConfirmPassword)
+            .NotEmpty().WithMessage("Please confirm your password")
+            .Equal(x => x.Password).WithMessage("Passwords do not match");
     }
 }
--- Features/Wizard/Steps/Personal/PersonalInfoValidator.cs
+++ Features/Wizard/Steps/Personal/PersonalInfoValidator.cs
@@ -6,8 +6,16 @@ public class PersonalInfoValidator : AbstractValidator<PersonalInfo>
 {
     public PersonalInfoValidator()
     {
-        RuleFor(x => x.FullName).NotEmpty().MinimumLength(2);
-        RuleFor(x => x.Email).NotEmpty().EmailAddress();
-        RuleFor(x => x.Phone).Matches(@"^\+?\d[\d\s\-()]*$").When(x => !string.IsNullOrEmpty(x.Phone));
+        RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("Full name is required")
+            .MinimumLength(2).WithMessage("Full name must be at least 2 characters");
+        
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Please enter a valid email address");
+        
+        RuleFor(x => x.Phone)
+            .NotEmpty().WithMessage("Phone is required")
+            .Matches(@"^\+?\d[\d\s\-()]*$").WithMessage("Please enter a valid phone number (e.g., +1 234 567 890)");
     }
 }
